4c8eb4becb919594f68936335b445f49915464b0
Tiago <tiago@>
2025-05-29 21:13:41 -0300

    Adicionado algumas classe já feitas mas ainda não implementadas, falta a princípio apenas mais um classe gerenciadora e finzalir e adicioanar os diagramas de classe

--- app/src/main/java/biblioteca/Leitor.java
+++ app/src/main/java/biblioteca/Leitor.java
@@ -9,12 +9,55 @@ public class Leitor {
     private String nome;
     private String endereco;
     private String telefone;
-    private List<Emprestimo> emprestimos;
+    private List<Emprestimo> emprestimos; // cada leito tem uma lista de emprestimos
 
     public Leitor(String nome, String endereco, String telefone) {
         this.id = idCounter++; // Método para gerar um ID único
         this.nome = nome;
         this.endereco = endereco;
         this.telefone = telefone;
+        this.emprestimos = new ArrayList<>();
+
     }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public String getEndereco(){
+        return endereco;
+    }
+
+    public String getTelefone(){
+        return telefone;
+    }
+
+    public List<Emprestimo> getEmprestimos(){
+        return new ArrayList<>(emprestimos);    
+}
+
+    public void setEmprestimo(Emprestimo e){
+        emprestimos.add(e);
+    }
+
+    public int ativosEmprestimos(){
+        return emprestimos.size();
+
+    }
+
+    public boolean temLivro(Livro book){
+        for(Emprestimo e : emprestimos){
+            if(e.getLivro().equals(book)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    
+
 }
